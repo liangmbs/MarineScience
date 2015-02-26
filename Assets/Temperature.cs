4c77acd1a7aae90c8a6b7089370fa9e193329a30
BluShine <umbreon27@gmail.com>
2015-02-26 14:03:25 -0500

    Fish temperature simulation code
    Added a Temperature object to generate temperatures, a ThermalCurve to
    take parameters and calculate thermal performance, and a Fish to
    visually display this data as a growing fish.

---
+++ Assets/Temperature.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Temperature : MonoBehaviour {
+
+    public float baseTemp = 20;
+    public float variability = 20;
+    public float cycleTime = 3;
+
+    private float currentTime = 0;
+
+    public float currentTemp;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentTime += Time.deltaTime;
+
+        float realCycle = cycleTime / (Mathf.PI * 2);
+
+        if (currentTime / realCycle > Mathf.PI * 2)
+        {
+            currentTime -= Mathf.PI * 2 * realCycle;
+        }
+        currentTemp = baseTemp + variability * Mathf.Sin(currentTime / realCycle);
+	}
+}
---
+++ Assets/fish.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fish : MonoBehaviour {
+
+    public ThermalCurve curve;
+    public float size = 50;
+    public float optimalGrowthPerSecond = 20;
+    public Temperature temperature;
+
+    public float swimmyness = 100;
+    public float swimmySpeed = 2f;
+    public float swimmyRandom = .01f;
+
+    private Vector3 startingPos;
+    private float swimX = 0;
+    private float swimY = 0;
+    private float randomX = 0;
+    private float randomY = 0;
+
+	// Use this for initialization
+	void Start () {
+        startingPos = transform.position;
+        swimX = Random.value * Mathf.PI * 2;
+        swimY = Random.value * Mathf.PI * 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float rate = curve.getCurve(273 + temperature.currentTemp) * swimmySpeed;
+
+        size += optimalGrowthPerSecond * Time.deltaTime * rate;
+
+        transform.localScale = new Vector3(size, size, 0);
+
+        //swim
+        randomX += (Random.value - .5f) / 10;
+        randomY += (Random.value - .5f) / 10;
+        if (randomX > 1)
+            randomX = 1;
+        if (randomX < -1)
+            randomX = -1;
+        if (randomY > 1)
+            randomY = 1;
+        if (randomY < -1)
+            randomY = -1;
+
+        swimX += rate * Time.deltaTime + randomX * swimmyRandom;
+        swimY += rate * Time.deltaTime + randomY * swimmyRandom;
+        if (swimX > Mathf.PI * 2)
+            swimX -= Mathf.PI * 2;
+        if (swimY > Mathf.PI * 2)
+            swimY -= Mathf.PI * 2;
+
+        transform.position = startingPos + new Vector3(Mathf.Sin(swimX) * swimmyness, Mathf.Sin(swimY) * swimmyness, 0);
+	}
+}
