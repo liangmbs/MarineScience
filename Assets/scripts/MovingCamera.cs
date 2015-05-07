7fa6f3869921a9eba75c349a8fed6c4accea4a6d
liangmbs <liang.chis@husky.neu.edu>
2015-05-07 16:46:53 -0400

    User Interface

---
+++ Assets/scripts/MovingCamera.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingCamera: MonoBehaviour {
+
+protected float HorizontalSpeed =5.0f;
+protected float VerticalSpeed =5.0f;
+
+public BoxCollider Background;
+private Vector3 min,max; 
+
+
+void Start(){
+	min = Background.bounds.min;
+	max = Background.bounds.max;
+}
+
+
+void LateUpdate(){
+	
+	if (Input.GetButton ("Fire1")) {
+		
+		float h = HorizontalSpeed * Input.GetAxis ("Mouse Y");
+		float v = VerticalSpeed * Input.GetAxis ("Mouse X");
+		
+		transform.Translate (v, h, 0);	
+	}
+	
+	if (Input.GetAxis ("Mouse ScrollWheel") >0) {
+		if(Camera.main.orthographicSize < 5)
+			Camera.main.orthographicSize++;
+		else{
+			
+		}
+	}
+	
+	if (Input.GetAxis ("Mouse ScrollWheel") <0) {
+		if(Camera.main.orthographicSize > 2)
+			Camera.main.orthographicSize --;
+		else{
+			
+		}
+	}
+
+	transform.position = new Vector3 (
+		Mathf.Clamp (transform.position.x, min.x , max.x ),
+		Mathf.Clamp (transform.position.y, min.y , max.y),
+		Mathf.Clamp (transform.position.z, min.z, max.z));
+}
+}
