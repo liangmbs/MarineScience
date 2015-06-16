61be9e63ac3c5bb71a6c321c3c0a497b333f9fc1
BluShine <umbreon27@gmail.com>
2015-06-16 16:01:49 -0400

    Revert "local change"
    This reverts commit 113f63e94cb460a02ea94cc7164cb0a853c0eff6, reversing
    changes made to ca6d29884f39d78ad5cfba96b7fa65ffee3cab89.

--- Assets/scripts/ShopWindowControl.cs
+++ Assets/scripts/ShopWindowControl.cs
@@ -27,7 +27,7 @@ public class ShopWindowControl : MonoBehaviour {
 		rt = (RectTransform)window.transform;
 		controler.GetComponent<Button>().
 			onClick.AddListener (() => Controler());
-        float homeX = rt.anchoredPosition.x;
+        float startX = rt.anchoredPosition.x;
 	}
 
     void Update()
@@ -43,18 +43,24 @@ public class ShopWindowControl : MonoBehaviour {
     }
 
 	void OnGUI(){
+		/*controler.GetComponent<Button>().
+			onClick.AddListener (() => Controler());*/
 		if (open == true) {
 			controler.transform.rotation = Quaternion.Euler(0,0,180);
 			rt.anchoredPosition = new Vector2
-                (Mathf.Lerp(-slideAmount, slideAmount, 1 - (slideTimer / slideTime)),
+				(Mathf.Lerp (homeX - slideAmount, homeX, 1 - (slideTimer / slideTime)),
                  rt.anchoredPosition.y);
+			//GameObject.Find("Main Camera").GetComponent<MovingCamera>().enabled = false;
+
 		}
 
 		if (open == false) {
 			controler.transform.rotation = Quaternion.Euler(0,0,0);
             rt.anchoredPosition = new Vector2
-                (Mathf.Lerp(slideAmount, -slideAmount, 1 - (slideTimer / slideTime)),
+                (Mathf.Lerp(homeX, homeX - slideAmount, 1 - (slideTimer / slideTime)),
                  rt.anchoredPosition.y);
+			//GameObject.Find("Main Camera").GetComponent<MovingCamera>().enabled = true;
+
 		}
 	}
 
